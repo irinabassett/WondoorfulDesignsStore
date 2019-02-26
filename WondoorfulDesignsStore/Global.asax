17048eb4fa809ca520d0ce2cdc2e5e8960021968
Irina Bassett <irina.bassett@gmail.com>
2019-02-26 18:42:30 -0500

    Add project files.

---
+++ WondoorfulDesignsStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WondoorfulDesignsStore.Global" Language="C#" %>
---
+++ WondoorfulDesignsStore/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WondoorfulDesignsStore.Startup))]
+namespace WondoorfulDesignsStore
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
