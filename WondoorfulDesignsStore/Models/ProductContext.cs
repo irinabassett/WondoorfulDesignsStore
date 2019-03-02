d3d8691b525f8f1bc0df1443c14885a1a08061bc
Irina Bassett <irina.bassett@gmail.com>
2019-03-02 18:35:52 -0500

    Created new folder Checkout with 5 new web pages: CheckoutStart, CheckoutReview, CheckoutComplete, CheckoutCancel, and CheckoutError. Addedwebconfig file to restrict unknown user acces.

--- WondoorfulDesignsStore/Models/ProductContext.cs
+++ WondoorfulDesignsStore/Models/ProductContext.cs
@@ -10,6 +10,8 @@ namespace WondoorfulDesignsStore.Models
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<CartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
 
