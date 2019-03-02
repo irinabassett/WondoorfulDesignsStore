f3729d2418248ad9b31c96f6721889144a9157ef
Irina Bassett <irina.bassett@gmail.com>
2019-03-02 15:53:07 -0500

    Add code to the ShoppingCart.aspx.cs code-behind to retrieve and populate the Shopping Cart UI.

--- WondoorfulDesignsStore/ShoppingCart.aspx.cs
+++ WondoorfulDesignsStore/ShoppingCart.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WondoorfulDesignsStore.Models;
+using WondoorfulDesignsStore.Logic;
 
 namespace WondoorfulDesignsStore
 {
@@ -12,6 +14,12 @@ namespace WondoorfulDesignsStore
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public List<CartItem> GetShoppingCartItems()
+        {
+            ShoppingCartActions actions = new ShoppingCartActions();
+            return actions.GetCartItems();
         }
     }
 }
